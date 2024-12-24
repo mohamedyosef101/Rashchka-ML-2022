8574e049545c8aeec8bd5bf65e6345cd79e42d9c
Mohamed Yosef <mohamedyosef101@outlook.com>
2024-12-25 01:00:50 +0200

    try the perceptron in the playground

--- playground.py
+++ playground.py
@@ -1,6 +1,23 @@
-from tools import data_downloader 
+from tools import split_iris
+from models import Perceptron
+import matplotlib.pyplot as plt 
 
-url = 'https://archive.ics.uci.edu/ml/' \
-'machine-learning-databases/iris/iris.data'
+X, y = split_iris('setosa', 'versicolor', [0, 2])
+ppn = Perceptron(eta=0.1, epochs=10, store_results=True)
+ppn.fit(X, y)
 
-data_downloader.download_data(url, 'iris.csv')
+def plot(lst, label):
+  plt.plot(range(1, len(lst) + 1),
+         lst, marker='o', label=label)
+
+plot(ppn.errors, "Errors")
+# plt.title("Errors over epochs")
+# plt.xlabel('Epochs')
+# plt.show()
+
+for i in range(len(ppn.weights[0])):
+  plot([w[i] for w in ppn.weights], f"w{i+1}")
+plot(ppn.biases, "biases")
+plt.xlabel('Epochs')
+plt.legend()
+plt.show()
